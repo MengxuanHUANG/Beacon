eb19c3beb1c9c4cdcacf3e2b0aba9e0e97407d47
Mengxuan HUANG <18082628d@connect.polyu.hk>
2022-01-02 16:49:51 +0800

    enable ApexDestruction Plugin

--- Source/Beacon/Beacon.Build.cs
+++ Source/Beacon/Beacon.Build.cs
@@ -26,6 +26,7 @@ public class Beacon : ModuleRules
 			new string[]
 			{
 				"Core",
+				"ApexDestruction",
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
